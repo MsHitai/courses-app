93fb132d23aa8df764a04296191c7684bb2811fb
MsHitai <mmalafeeva@yandex.ru>
2023-09-30 13:23:42 +0300

    initial commit

---
+++ courses/src/main/java/ru/trush/courses/OnlineCourseApp.java
@@ -0,0 +1,13 @@
+package ru.trush.courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCourseApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCourseApp.class, args);
+	}
+
+}
---
+++ courses/src/test/java/ru/trush/courses/OnlineCourseAppTests.java
@@ -0,0 +1,13 @@
+package ru.trush.courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCourseAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
